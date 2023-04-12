18e74ba46b5cc3bf2e07943d731921101043a020
TestyEldrich <olzhas2106@gmail.com>
2023-04-12 20:17:05 +0600

    Created interfaces for providers

---
+++ InventoryApp.DataAccess/Providers/Interfaces/IClassroomProvider.cs
@@ -0,0 +1,13 @@
+﻿using InventoryApp.Contracts.Responses;
+using InventoryApp.Models;
+using InventoryApp.Models.ResultModels;
+
+namespace InventoryApp.DataAccess.Providers.Interfaces
+{
+    public interface IClassroomProvider : IProvider<Classroom, Guid>
+    {
+        Task<List<SearchResult>> SearchClassroomByQuery(string category, int numberOfItems);
+        Task<List<StatisticsCategoryPerClassResult>> StatisticsPerClassCategory(string classroom);
+        Task<List<GetClassroomsNameResponse>> GetClassroomsName();
+    }
+}
---
+++ InventoryApp.DataAccess/Providers/Interfaces/ItemProvider.cs
@@ -0,0 +1,11 @@
+﻿using InventoryApp.Models;
+using static InventoryApp.DataAccess.Providers.EntityProviders.EntityItemProvider;
+
+namespace InventoryApp.DataAccess.Providers.Interfaces
+{
+    public interface ItemProvider : IProvider<Item, Guid>
+    {
+        Task<List<object>> GetAllItems(Guid id);
+        Task<object> GetOneById(Guid id);
+    }
+}
