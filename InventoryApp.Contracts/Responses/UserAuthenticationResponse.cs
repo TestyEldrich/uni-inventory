1b6b9cb48cf6b2252cf266b4ff091c765d558e4f
TestyEldrich <olzhas2106@gmail.com>
2023-04-14 16:53:42 +0600

    Contracts for requests done

---
+++ InventoryApp.Contracts/Responses/UserAuthenticationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryApp.Contracts.Responses
+{
+    public class UserAuthenticationResponse
+    {
+        public string Token { get; set; }
+        public int? Role { get; set; }
+    }
+}
---
+++ InventoryApp.Contracts/Responses/UserGetIdentityResponse.cs
@@ -0,0 +1,14 @@
+﻿using InventoryApp.Models;
+
+namespace InventoryApp.Contracts.Responses
+{
+    public class UserGetIdentityResponse
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public List<Reports> ReportsUrl { get; set; }
+
+    }
+}
