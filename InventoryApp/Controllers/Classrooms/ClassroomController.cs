1644d5441d68e67c3a3687fe93519065275ac8d8
TestyEldrich <olzhas2106@gmail.com>
2023-04-22 13:48:52 +0600

    Classroom, item and report controllers

---
+++ InventoryApp/Controllers/Classrooms/ClassroomController.cs
@@ -0,0 +1,201 @@
+﻿using InventoryApp.AppStart.Filters;
+using InventoryApp.Contracts.Attributes;
+using InventoryApp.Contracts.Parameters.Classroom;
+using InventoryApp.DataAccess.Providers.Interfaces;
+using InventoryApp.Models;
+using InventoryApp.Models.Users.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Text.RegularExpressions;
+
+namespace InventoryApp.Controllers.Classrooms
+{
+    [ApiExplorerSettings(GroupName = "v1-admin")]
+    [Route("api/classroom/")]
+    public class ClassroomController : Controller
+    {
+        private readonly IClassroomProvider _classroomProvider;
+        private readonly ItemProvider _itemProvider;
+        public ClassroomController(IClassroomProvider classroomProvider,
+            ItemProvider itemProvider)
+        {
+            _classroomProvider = classroomProvider;
+            _itemProvider = itemProvider;
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin)]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ClassroomCreateParameter classroomCreate)
+        {
+            try
+            {
+                var classroom = new Classroom
+                {
+                    ClassroomName = classroomCreate.ClassroomName,
+                    IconUrl = classroomCreate.IconUrl,
+                    Description = classroomCreate.ClassroomDescription
+                };
+
+                await _classroomProvider.Add(classroom);
+
+                return Ok(new { message = "Classroom has been created successfully!" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Classroom with given number/name is already added!" });
+            }
+        }
+
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var classroom = await _classroomProvider.GetById(id);
+
+                return Ok(classroom);
+            } catch (Exception ex) { 
+                return BadRequest(ex.Message);
+            }
+        }
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet]
+        public async Task<IActionResult> GetAllClassrooms()
+        {
+            try
+            {
+                var result = await _classroomProvider.GetAll();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin)]
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Edit(Guid id, [FromBody] ClassroomEditParameter classroomEditParameter)
+        {
+            try
+            {
+                var checkClass = await _classroomProvider.FirstOrDefault(x =>
+                    x.ClassroomName.Equals(classroomEditParameter.ClassroomName));
+                var editClassroom = await _classroomProvider.GetById(id) ??
+                    throw new Exception("No such class");
+
+                if (checkClass is not null)
+                {
+                    if (!checkClass.Id.Equals(editClassroom.Id))
+                    {
+                        return BadRequest("Classroom with current name/number is existing!");
+                    }
+                }
+
+                editClassroom.ClassroomName = classroomEditParameter.ClassroomName;
+                editClassroom.Description = classroomEditParameter.ClassroomDescription;
+                editClassroom.IconUrl = classroomEditParameter.IconUrl;
+
+                await _classroomProvider.Edit(editClassroom);
+
+                return Ok(new { message = "Classroom has been edited successfully!"});
+
+            } catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin)]
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                var classroom = await _classroomProvider.GetById(id);
+
+                if (classroom is null) return NotFound();
+                
+                await _classroomProvider.Remove(classroom);
+                var forDelete = await _itemProvider.Get(x => x.ClassroomId == id);
+                if (forDelete is null) return Ok();
+
+                await _itemProvider.RemoveRange(forDelete);
+
+                return Ok(new {message = "Classroom has been deleted successfully"});
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet("search/{category}/{numberofitems:int}")]
+        public async Task<IActionResult> Search(string category, int numberOfItems)
+        {
+            try
+            {
+                Console.WriteLine(category);
+                var result = await _classroomProvider.SearchClassroomByQuery(category, numberOfItems);
+
+                return Ok(result);
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet("search/{classroom}")]
+        public async Task<IActionResult> SearchByName(string classroom)
+        {
+            try
+            {
+                
+                var result = await _classroomProvider.Get(x => Regex.IsMatch(x.ClassroomName, classroom));
+
+                return Ok(result);
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+
+        }
+
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet("getclassroomsname")]
+        public async Task<IActionResult> GetNames()
+        {
+            try
+            {
+                var result = await _classroomProvider.GetClassroomsName();
+
+                return Ok(result);
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+        [HttpGet("stats/{id:guid}")]
+        public async Task<IActionResult> GetStats(Guid id)
+        {
+            try
+            {
+                var result = await _classroomProvider.StatisticsPerClassCategory(id.ToString());
+
+                return Ok(result);
+            } catch(Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
---
+++ InventoryApp/Controllers/Item/ItemController.cs
@@ -0,0 +1,284 @@
+﻿using Amazon.S3.Model.Internal.MarshallTransformations;
+using InventoryApp.AppStart.Filters;
+using InventoryApp.Contracts.Attributes;
+using InventoryApp.Contracts.Parameters.Item;
+using InventoryApp.DataAccess.Providers.Interfaces;
+using InventoryApp.Hubs;
+using InventoryApp.Models;
+using InventoryApp.Models.Users.User;
+using InventoryApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
+using System.Diagnostics.Contracts;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+namespace InventoryApp.Controllers.Item
+{
+    [ApiExplorerSettings(GroupName = "v1-admin")]
+    [Route("api/item/")]
+    [AdminAuthorized(UserRole.SuperAdmin, UserRole.Admin, UserRole.Moderator)]
+    public class ItemController : ControllerBase
+    {
+        private readonly ItemProvider _itemProvider;
+        private readonly IClassroomProvider _classroomProvider;
+        private readonly AwsS3FileUploadService _uploadService;
+        private readonly ImageProvider _imageProvider;
+        private readonly IHubContext<ClassroomHub> _hub;
+        private readonly IMemoryCache _cache;
+        public ItemController(ItemProvider itemProvider,
+            IClassroomProvider classroomProvider,
+            AwsS3FileUploadService uploadService,
+            ImageProvider imageProvider,
+            IHubContext<ClassroomHub> hub,
+            IMemoryCache cache) { 
+            _itemProvider = itemProvider;
+            _classroomProvider = classroomProvider;
+            _uploadService = uploadService;
+            _imageProvider= imageProvider;
+            _hub = hub;
+            _cache = cache;
+        }
+        [HttpGet("bynumber/{number}")]
+        public async Task<IActionResult> GetByNumber(string number)
+        {
+            try
+            {
+                var item = await _itemProvider.Get(x => x.ItemNumber.Equals(number));
+
+                return Ok(item);
+            } catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [HttpPost("fromscanner")]
+        public async Task<IActionResult> CreateFromScanner([FromBody] ItemCreateParameter itemCreateParameter)
+        {
+            try
+            {
+                var classroom = await _classroomProvider.FirstOrDefault(x => x.ClassroomName == itemCreateParameter.ClassroomId);
+
+                if (classroom is null)
+                    return NotFound(new { message = "There is no classes with given name or number!" });
+
+                var newItem = new Models.Item
+                {
+                    Name = itemCreateParameter.Name,
+                    Description = itemCreateParameter.Description,
+                    IconUrl = itemCreateParameter.IconUrl,
+                    Condition = itemCreateParameter.Condition,
+                    ClassroomId = classroom.Id,
+                    CategoryId = Guid.Parse(itemCreateParameter.CategoryId),
+                    ItemNumber = itemCreateParameter.ItemNumber,
+                };
+
+                await _itemProvider.Add(newItem);
+                await SendReload(classroom.Id.ToString());
+                return Ok(new { message = "Item was added successfully!" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpGet("getimages/{id:guid}")]
+        public async Task<IActionResult> GetImages(Guid id)
+        {
+            try
+            {
+                var result = await _imageProvider.Get(x => x.EntityId.Equals(id));
+
+                    
+                return Ok(result);
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                if(file is null)
+                {
+                    return Ok();
+                }
+                var result = await _uploadService.UploadFileAsync(file);
+
+                return Ok(new { fileUrl = result });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        [HttpPost("uploadmany")]
+        public async Task<IActionResult> UploadMany([FromForm]UploadItemsParameter parameter)
+        {
+            try
+            {
+                var range = new List<Image>();
+                var owner = parameter.Owner;
+
+                if (parameter.files.Count == 0)
+                {
+                    return Ok();
+                }
+
+                foreach (var file in parameter.files)
+                {
+                    Console.WriteLine(file.FileName);
+                    var result = await _uploadService.UploadFileAsync(file);
+                    range.Add(new Image { EntityId = Guid.Parse(owner) , Url = result});
+                }
+
+                await _imageProvider.AddRange(range);
+
+                return Ok(new { message = "Images were uploaded."});
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+        private async Task SendReload(string classroomId)
+        {
+            List<string>? list;
+
+            if (_cache.TryGetValue(classroomId, out list))
+            {
+                foreach(var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+                await _hub.Clients.Clients(list).SendAsync("SendReload");
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ItemCreateParameter itemCreateParameter)
+        {
+            try
+            {
+                var newItem = new Models.Item
+                {
+                    Name= itemCreateParameter.Name,
+                    Description= itemCreateParameter.Description,
+                    IconUrl= itemCreateParameter.IconUrl,
+                    Condition= itemCreateParameter.Condition,
+                    ClassroomId= Guid.Parse(itemCreateParameter.ClassroomId),
+                    CategoryId= Guid.Parse(itemCreateParameter.CategoryId),
+                    ItemNumber= itemCreateParameter.ItemNumber,
+                };
+
+                await _itemProvider.Add(newItem);
+                await SendReload(itemCreateParameter.ClassroomId);
+                return Ok(new { message = "Item was added successfully!" });
+            } catch(Exception ex) { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("classroom/{id:guid}")]
+        public async Task<IActionResult> GetAllItems(Guid id)
+        {
+            try
+            {
+                var result = await _itemProvider.GetAllItems(id);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("byid/{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var result = await _itemProvider.GetOneById(id);
+
+                return Ok(result);
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Edit(Guid id, [FromBody] ItemCreateParameter parameter)
+        {
+            try
+            {
+                var editItem = await _itemProvider.GetById(id) ??
+                    throw new Exception("No such item");
+                
+                var classroom = await _classroomProvider.FirstOrDefault(x => x.ClassroomName == parameter.ClassroomId);
+
+                if (classroom is null)
+                    return NotFound(new { message = "There is no classes with given name or number!" });
+
+                editItem.Name = parameter.Name;
+                editItem.Description = parameter.Description;
+                editItem.IconUrl = parameter.IconUrl;
+                editItem.Condition = parameter.Condition;
+                editItem.CategoryId = Guid.Parse(parameter.CategoryId);
+                editItem.ClassroomId = classroom.Id;
+                editItem.ItemNumber = parameter.ItemNumber;
+
+                await _itemProvider.Edit(editItem);
+
+                return Ok(new {message = "Item was edited successfully!"});
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ServiceFilter(typeof(UserActionAttribute))]
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                var item = await _itemProvider.GetById(id);
+                var classroomId = item.ClassroomId;
+                if (item is null) return NotFound();
+                
+                
+                await _itemProvider.Remove(item);
+                await SendReload(classroomId.ToString());
+                return Ok(new {message = "Item was deleted successfully!"});
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("search/{classroom}/{param}")]
+        public async Task<IActionResult> Search(string classroom, string param)
+        {
+            try
+            { 
+                var result = await _itemProvider.Get(x => (Regex.IsMatch(x.Name, param) && x.ClassroomId.Equals(Guid.Parse(classroom))));
+
+                return Ok(result);
+            }
+            catch (Exception) {
+                return BadRequest();
+            }
+        }
+    }
+}
